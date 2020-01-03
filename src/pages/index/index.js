33a2a534f8d02b97120441943e5c66a5d4ec4721
Xiaoyi <yi.xiao@pccw.com>
2020-01-03 12:07:35 +0800

    完成主页

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -3,18 +3,48 @@ import './index.css'
 import Vue from 'vue'
 import axios from 'axios'
 import url from 'js/api.js'
+import { InfiniteScroll } from 'mint-ui'
+
+Vue.use(InfiniteScroll)
+import foot from './../../components/foot.vue'
 let app = new Vue({
   el: '#app',
   data: {
-    lists: null
+    lists: null,
+    pageNum: 1,
+    pageSize: 6,
+    loading: false,
+    allLoading: false
   },
-  created() {
-    axios.get(url.hotLists, {
-      pageNum: 1,
-      pageSize: 6
-    }).then(res => {
-      this.lists = res.data.lists
-    })
+  created () {
+    this.getLists()
+  },
+  methods: {
+    getLists () {
+      if (this.allLoading) return
+      this.loading = true
+      axios.get(url.hotLists, {
+        pageNum: this.pageNum,
+        pageSize: this.pageSize
+      }).then(res => {
+        let curLists = res.data.lists
+        // 判断所有数据是否加载完毕
+        if (curLists.lenth < this.pageSize) {
+          this.allLoading = true
+        }
+        if (this.lists) {
+          this.lists = this.lists.concat(curLists)
+        } else {
+          // 第一次请求数据
+          this.lists = curLists
+        }
+        this.loading = false
+        this.pageNum++
+      })
+    }
+  },
+  components: {
+    foot:foot
   }
 })
 app.$mount('#app')
